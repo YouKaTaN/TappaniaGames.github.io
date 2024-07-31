c2dc0e301712e310b514acd04eb6bfb4161fa640
Artem <art.fedorenko@gmail.com>
2024-07-31 19:29:10 +0300

    VK_Build 1.16

---
+++ ChineseChess/lib/runtimeData.js
@@ -0,0 +1,36 @@
+const runtimeData = (function () {
+
+    return {
+
+        // Basic information.
+        companyName: "TappaniaGames",
+        productName: "YCC",
+        productVersion: "1.16",
+        sdkVersion: "3.16.3",
+        productDescription: "",
+
+        // File references.
+        buildURL: "bin",
+        loaderURL: "bin/Web_VKontakte_temp.loader.js",
+        dataURL: "bin/Web_VKontakte_temp.data.unityweb",
+        frameworkURL: "bin/Web_VKontakte_temp.framework.js.unityweb",
+        workerURL: "",
+        codeURL: "bin/Web_VKontakte_temp.wasm.unityweb",
+        symbolsURL: "",
+        streamingURL: "streaming",
+
+        // Aspect ratio.
+        desktopAspectRatio: -1,
+        mobileAspectRatio: -1,
+
+        // Debug mode.
+        debugMode: false,
+
+        // Platform specific scripts.
+        wrapperScript: "vkontakteWrapper.js",
+        gameDistributionId: "",
+        gameDistributionPrefix: "mirragames_",
+
+    }
+
+})();
