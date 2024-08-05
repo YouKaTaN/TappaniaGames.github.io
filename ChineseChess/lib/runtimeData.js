b31f7377080feb406d243e237b485a2f1b38f321
Artem <art.fedorenko@gmail.com>
2024-08-06 01:10:36 +0300

    1.41 1000x1776

--- ChineseChess/lib/runtimeData.js
+++ ChineseChess/lib/runtimeData.js
@@ -5,7 +5,7 @@ const runtimeData = (function () {
         // Basic information.
         companyName: "TappaniaGames",
         productName: "YCC",
-        productVersion: "1.40",
+        productVersion: "1.41",
         sdkVersion: "3.16.3",
         productDescription: "",
 
@@ -21,7 +21,7 @@ const runtimeData = (function () {
 
         // Aspect ratio.
         desktopAspectRatio: 1.666667,
-        mobileAspectRatio: 0.6,
+        mobileAspectRatio: 0.5630631,
 
         // Debug mode.
         debugMode: false,
