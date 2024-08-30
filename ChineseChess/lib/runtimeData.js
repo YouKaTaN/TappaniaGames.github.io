1c250a85146120c149bc8cbed2c04af20aca6cd6
Artem <art.fedorenko@gmail.com>
2024-08-30 18:14:10 +0300

    1.17.9 Test Saves

--- ChineseChess/lib/runtimeData.js
+++ ChineseChess/lib/runtimeData.js
@@ -5,17 +5,17 @@ const runtimeData = (function () {
         // Basic information.
         companyName: "TappaniaGames",
         productName: "YCC",
-        productVersion: "1.17.8",
-        sdkVersion: "3.17.11",
+        productVersion: "1.17.9",
+        sdkVersion: "3.17.14",
         productDescription: "",
 
         // File references.
         buildURL: "bin",
-        loaderURL: "bin/ChineseChess_Web_VKontakte.loader.js",
-        dataURL: "bin/ChineseChess_Web_VKontakte.data.unityweb",
-        frameworkURL: "bin/ChineseChess_Web_VKontakte.framework.js.unityweb",
+        loaderURL: "bin/CC_Web_VKontakte.loader.js",
+        dataURL: "bin/CC_Web_VKontakte.data.unityweb",
+        frameworkURL: "bin/CC_Web_VKontakte.framework.js.unityweb",
         workerURL: "",
-        codeURL: "bin/ChineseChess_Web_VKontakte.wasm.unityweb",
+        codeURL: "bin/CC_Web_VKontakte.wasm.unityweb",
         symbolsURL: "",
         streamingURL: "streaming",
 
